54de803b1d440844ffcc09c7985090854b3fc7df
Amirhossein Abedi <amirhoseinabedi80@gmail.com>
2024-10-13 14:59:02 +0330

    Added books grpc service

--- books_service/caller.py
+++ books_service/caller.py
@@ -34,31 +34,24 @@ class Book(Base):
     def __repr__(self) -> str:
         return f"{self.id}"
 
-Base.metadata.create_all(engine)
+# Base.metadata.create_all(engine)
 
-Session = sessionmaker(bind=engine)
-session = Session()
+# Session = sessionmaker(bind=engine)
+# session = Session()
 
 class Books(books_pb2_grpc.BooksServiceServicer):
     def is_book(self, request, context):
         res = session.query(session.query(Book).filter(Book.id == request.book_id).exists()).scalar()
-        return books_pb2.IsBookResponse(exists=res)
+        return books_pb2.IsBookResponse(res)
 
-    def is_book_by_name(self, request, context):
-        res = session.query(session.query(Book).filter(Book.title == request.book_name).exists()).scalar()
-        return books_pb2.IsBookResponse(exists=res)
-    
     def get_book_by_id(self, request, context):
         book = session.query(Book).filter(Book.id == request.book_id).first()
-        return books_pb2.Book(title=book.title, author=book.author, genre=book.genre, id=book.id)
-
-    def get_book_by_name(self, request, context):
-        book = session.query(Book).filter(Book.title == request.book_name).first()
-        return books_pb2.Book(title=book.title, author=book.author, genre=book.genre, id=book.id)
+        return books_pb2.Book(book.title, book.author, book.genre)
 
     def all_books(self, request, context):
+        return books_pb2.AllBooksResponse([Book(title="A", author="B", genre="C")])
         arr = session.query(Book).all()
-        return books_pb2.AllBooksResponse(books=[books_pb2.Book(title=i.title, author=i.author, genre=i.genre, id=i.id) for i in arr])
+        return books_pb2.AllBooksResponse(books=[Book(i.title, i.author, i.genre) for i in arr])
 
     def add_book(self, request, context):
         book = Book(title=request.title, author=request.title, genre=request.genre)
@@ -84,15 +77,21 @@ class Books(books_pb2_grpc.BooksServiceServicer):
         session.commit()
         return books_pb2.EmptyObject()
     
-def serve():
-    b1 = Book(title="Amir", author="Amir2", genre="aaaa")
-    session.add(b1)
-    session.commit()
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    books_pb2_grpc.add_BooksServiceServicer_to_server(Books(), server)
-    server.add_insecure_port('[::]:50051')
-    server.start()
-    server.wait_for_termination()
+# def serve():
+#     b1 = Book(title="Amir", author="Amir2", genre="aaaa")
+#     session.add(b1)
+#     session.commit()
+#     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+#     books_pb2_grpc.add_BooksServiceServicer_to_server(Books(), server)
+#     server.add_insecure_port('[::]:50051')
+#     server.start()
+#     server.wait_for_termination()
 
-if __name__=="__main__":
-    serve()
+# if __name__=="__main__":
+#     serve()
+
+with grpc.insecure_channel('localhost:50051') as channel:
+    response = books_pb2_grpc.BooksServiceStub(channel).all_books(books_pb2.EmptyObject())
+    for i in response.books:
+        print(type(i.title))
+    print([{i:i} for i in range(10)])
